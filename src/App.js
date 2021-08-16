f5bd164109058a12d3fa9c416daf3d0e7006c588
Aagman Suri <aagmansuri96@gmail.com>
2021-08-16 17:16:07 +0530

    title , data , random no added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-
-
+import { useState, useEffect } from "react";
+import Photo from "./Photo";
 function App() {
+  const AlbumsUrl = "https://jsonplaceholder.typicode.com/albums";
+  const PhotosUrl = "https://jsonplaceholder.typicode.com/photos";
+  const [albumData, setAlbumData] = useState([]);
+  const [PhotoData, setPhotoData] = useState([]);
+
+  useEffect(() => {
+    fetch(AlbumsUrl)
+      .then((response) => response.json())
+      .then((data) => setAlbumData(data));
+    fetch(PhotosUrl)
+      .then((response) => response.json())
+      .then((data) => setPhotoData(data));
+  }, []);
+
+  console.log(PhotoData);
   return (
     <div className="App">
-      <h1>Hello</h1>
+      {albumData
+        .filter((album) => {
+          return album.id <= 5;
+        })
+        .map((filterAlbum) => {
+          return (
+            <div key={filterAlbum.id}>
+              <h1>{filterAlbum.title} </h1>
+
+              {PhotoData.map((item) => {
+                if (item.albumId === filterAlbum.id) {
+                  return (
+                    <div>
+                      <p>{item.title}</p>
+                      <p>${Math.floor(Math.random() * 250) + 50}</p>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          );
+        })}
     </div>
   );
 }
 
 export default App;
+
+// {/* <div key={album.id}>
+// <h5>{album.title}</h5>
+// </div> */}
+
+// {
+//   /* <h6>{Math.floor(Math.random() * 250) + 50}</h6> */
+// }
+// {
+//   /* {PhotoData.map((photo) => {
+//               return <div></div>;
+//             })} */
+// }
